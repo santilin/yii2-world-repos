18c0d2d73a3fe001319c031ee1644604ec2bee3d
Santilín <software@noviolento.es>
2023-08-15 15:47:07 +0200

    PlaceController

---
+++ controllers/PlaceController.php
@@ -0,0 +1,25 @@
+<?php
+/*<<<<<USES*/
+/*Template:Yii2App/controllers/EmptyController.php*/
+namespace santilin\wrepos\\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\Response;
+/**
+ * PlaceController an empty controller.
+ */
+class PlaceController extends Controller
+{
+	/**
+	 * @var $layout The layout for this controller
+	 */
+	public $layout = 'main';
+	/**
+	 * @inheritdoc
+	 */
+    public $enableCsrfValidation = false;
+/*>>>>>USES*/
+/*<<<<<CLASS_END*/
+} // class SiteController
+/*>>>>>CLASS_END*/
