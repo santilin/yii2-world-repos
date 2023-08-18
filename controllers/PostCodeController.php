dffb0fb88a3a7a62b18a6a96cb9267c95d4f5922
santilin <franciscocapel@cepaim.org>
2023-08-18 15:11:25 +0200

    PostCodeController:findTypeAhead

--- controllers/PostCodeController.php
+++ controllers/PostCodeController.php
@@ -23,7 +23,27 @@ class PostCodeController extends Controller
 /*>>>>>CLASS*/
 
 	// Cant use Query because postcode has no id
-	public function actionFind(string $search, int $page = 1, int $per_page = 5)
+// 	public function actionFind(string $search, int $page = 1, int $per_page = 5)
+// 	{
+// 		if( !$page) {
+// 			$page = 1;
+// 		}
+// 		\Yii::$app->response->format = Response::FORMAT_JSON;
+// 		$sql = "SELECT pc.postcode as id, pc.postcode, pl.name FROM "
+// 		. PostCode::tableName() . 'pc INNER JOIN '
+// 		. Place::tableName() . 'pl ON pl.id=pc.places_id'
+// 		. " WHERE pc.postcode = :postcode OR pl.name LIKE :postcode_like";
+// 		if ($page) {
+// 			$sql .= ' LIMIT ' . ($page-1) * $per_page . ",$per_page";
+// 		}
+// 		$models = PostCode::getDb()->createCommand($sql)
+// 			->bindValue(':postcode', $search)
+// 			->bindValue(':postcode_like', $search . '%')
+// 			->queryAll();
+// 		return  [ 'items' => $models, 'total_count' => 20 ];
+// 	}
+//
+	public function actionFindTypeAhead(string $search, int $page = 1, int $per_page = 5)
 	{
 		if( !$page) {
 			$page = 1;
@@ -40,8 +60,14 @@ class PostCodeController extends Controller
 			->bindValue(':postcode', $search)
 			->bindValue(':postcode_like', $search . '%')
 			->queryAll();
-		return  [ 'items' => $models, 'total_count' => 20 ];
+		$ret = [];
+		foreach ($models as $model) {
+			$ret[] = [ 'value' => $model['name'], 'id' => $model['id'] ];
+		}
+		return $ret;
 	}
+
+
 
 
 	// Cant use Query because postcode has no id
