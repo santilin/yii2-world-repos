d8bc93298c7ad744777a86f9b84460e84042b220
user <user@git.com>
2024-07-16 19:07:51 +0200

    cappel: added `Place._grid` default form
    cappel: added `Place._view` default form
    
    cappel: added `Place._form` default form

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,10 +9,6 @@ use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\web\Response;
 /*>>>>>USES*/
-/*<<<<<DATECONTROL_MAIN*/
-use DateTimeZone;
-use kartik\datecontrol\DateControl;
-/*>>>>>DATECONTROL_MAIN*/
 /*<<<<<MAIN*/
 class SiteController extends Controller
 {
@@ -91,48 +87,6 @@ class SiteController extends Controller
 		return $this->render('index', $params);
 	}
 /*>>>>>INDEX_END_PAGE*/
-/*<<<<<DATECONTROL_ACTIONCONVERT*/
-    /**
-     * Convert display date for saving to model.
-     * Customized to allow datetimes without the time part
-     *
-     * @return array JSON encoded HTML output
-     */
-    public function actionConvertDate()
-    {
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $req = Yii::$app->request;
-        $post = $req->post();
-        if (!isset($post['displayDate'])) {
-            return ['status' => 'error', 'output' => 'No display date found'];
-        }
-        $saveFormat = $req->post('saveFormat', '');
-        $saveTimezone = $req->post('saveTimezone', '');
-        $dispFormat = $req->post('dispFormat', '');
-        $dispTimezone = $req->post('dispTimezone', '');
-        $displayDate = trim($req->post('displayDate', ''));
-        $settings = $req->post('settings', []);
-        $type = $req->post('type');
-        $date = DateControl::getTimestamp($displayDate, $dispFormat, $dispTimezone, $settings);
-        if (empty($date) || !$date) {
-			if ($type == DateControl::FORMAT_DATETIME) {
-				$onlyDateFormat = Yii::$app->getModule('datecontrol')->getDisplayFormat(DateControl::FORMAT_DATE);
-				// Try to parse only date
-				$date = DateControl::getTimestamp($displayDate, $onlyDateFormat, null, $settings);
-				$displayDate = $date->setTime(12,0,0)->format($dispFormat); // compat wit datetimevalidator
-				$date = DateControl::getTimestamp($displayDate, $dispFormat, $dispTimezone, $settings);
-			}
-		}
-        if (empty($date) || !$date) {
-            $value = '';
-        } elseif ($saveTimezone != null) {
-            $value = $date->setTimezone(new DateTimeZone($saveTimezone))->format($saveFormat);
-        } else {
-            $value = $date->format($saveFormat);
-        }
-        return ['status' => 'success', 'output' => $value];
-    }
-/*>>>>>DATECONTROL_ACTIONCONVERT*/
 /*<<<<<CLASS_END*/
 } // class SiteController
 /*>>>>>CLASS_END*/
---
+++ forms/Place_Form.php
@@ -0,0 +1,72 @@
+<?php
+/*<<<<<USES*/
+/*Template:Yii2App/models/ModelForm.php*/
+namespace santilin\wreposforms;
+
+use Yii;
+use santilin\wrepos\models\Place;
+use santilin\churros\helpers\FormHelper;
+/*>>>>>USES*/
+/*<<<<<CLASS*/
+/**
+ * This is a custom form class for model Place
+ *
+ */
+class Place_Form extends Place
+{
+/*>>>>>CLASS*/
+/*<<<<<RULES*/
+    public function rules()
+    {
+		$rules = array_merge(parent::rules(), [
+			'f_safe'=>[['id'], 'safe', 'on' => $this->getCrudScenarios()],
+			'req' => [['name','level','countries_id'], 'required', 'on' => $this->getCrudScenarios()],
+			'null' => [['name_es','name_en','name_fr','admin_code','admin_sup_code','admin_sup_name','national_id'], 'default', 'value' => null, 'on' => $this->getCrudScenarios()],
+		]);
+/*>>>>>RULES*/
+/*<<<<<RULES_RETURN*/
+		return $rules;
+    } // rules
+/*>>>>>RULES_RETURN*/
+/*<<<<<AFTER_SAVE*/
+	public function afterSave($insert, $changedAttributes)
+	{
+		parent::afterSave($insert, $changedAttributes);
+		$fdf = FormHelper::getConfig('Place_Form', 'DefaultValues', []);
+		if( is_array($fdf) ) {
+			$df = [];
+			foreach($fdf as $fld => $value) {
+				$df[$fld] = $this->$fld;
+			}
+			if( !empty($df) ) {
+				FormHelper::setConfig('Place_Form', 'DefaultValues', $df);
+			}
+		} else {
+			FormHelper::setConfig('Place_Form', 'DefaultValues', []);
+		}
+/*>>>>>AFTER_SAVE*/
+/*<<<<<AFTER_SAVE.END*/
+	}
+/*>>>>>AFTER_SAVE.END*/
+/*<<<<<DEFAULT_VALUES*/
+	public function setDefaultValues(bool $duplicating = false)
+	{
+		parent::setDefaultValues($duplicating);
+/*>>>>>DEFAULT_VALUES*/
+/*<<<<<DEFAULT_VALUES.PARENT*/
+
+		if (!$duplicating) { // dont set these default values while duplicating
+
+		}
+		$fdf = FormHelper::getConfig('Place_Form', 'DefaultValues', []);
+		if( !empty($fdf) ) {
+			$this->setAttributes($fdf);
+		}
+/*>>>>>DEFAULT_VALUES.PARENT*/
+/*<<<<<DEFAULT_VALUES.RETURN*/
+	} // setDefaultValues
+/*>>>>>DEFAULT_VALUES.RETURN*/
+/*<<<<<END*/
+} // class Place_Form
+/*>>>>>END*/
+		// customize your great rules here
