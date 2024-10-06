9f827fab6f657bba15b840e3ddc4e349f0e2c679
Santilín <software@noviolento.es>
2024-10-06 15:33:50 +0200

    added secrets.php

---
+++ config/secrets.php
@@ -0,0 +1,12 @@
+<?php
+$cvk = 'Эг~qВ>Ќiиц~ЇHDM,ХЗ
+BИBЖ·!+аЭЭFэ';
+/*<<<<<CONFIG*/
+$secrets = [
+	'cookie_validation_key' => $cvk,
+];
+unset($cvk);
+/*>>>>>CONFIG*/
+/*<<<<<RETURN*/
+return $secrets;
+/*>>>>>RETURN*/
